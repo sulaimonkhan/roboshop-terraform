783f5c816e7443ff6662031c65c77a6fb33965cc
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-12-14 14:14:24 +0530

    roboshop-terraform

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,24 +3,24 @@ resource "aws_security_group" "sg" {
   description = "Inbound allow for $ {var.component_name}"
   
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "TCP"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   
   ingress {
-    from_port        = var.app_port
-    to_port          = var.app_port
-    protocol         = "TCP"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]   
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]   
   }
 }
 
