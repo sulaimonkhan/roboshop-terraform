3954f74603aa1e3aeeb7067bc3069ad18b7eb267
sulaimonkhan <sulaimonkhan04@gmail.com>
2025-03-04 15:46:37 +0530

    terraform

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -33,14 +33,21 @@ resource "aws_instance" "instance" {
     Name = "${var.component_name}-${var.env}"
   }
   
-  
-  provisioner "local-exec" {
-    command = <<EOL
-cd /home/ec2-user/roboshop-ansible
-ansible-playbook -i ${self.private_ip}, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e app_name=${var.component_name} -e env=${var.env} roboshop.yml
-EOL      
+  provisioner "remote-exec" {
 
+     connection {
+       type     = "ssh"
+       user     = "ec2-user"
+       password = "DevOps321"
+       host     = self.private_ip 
+     } 
+
+     inline = [
+       "sudo labauto ansible",
+       "ansible-pull -i localhost, -U https://github.com/sulaimonkhan/roboshop-ansible roboshop.yml -e env=${var.env} -e app_name=${var.component_name}"
+     ]
   }
+ 
 
 }    
    
